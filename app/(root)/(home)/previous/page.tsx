883b2b1fbac322c6c9a7172a0764182601a499f6
Akanksha <akankshaprasad184@gmail.com>
2024-04-19 22:45:25 +0530

    Recordings join meets

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import CallList from "@/components/CallList";
 
-const Previous = () => {
+const PreviousPage = () => {
   return (
-    <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>Previous Meetings</h1>
-      <p className='text-lg'>Welcome to the Previous Meetings</p>
-    </section>
-  )
-}
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Previous Calls</h1>
 
-export default Previous;
+      <CallList type="ended" />
+    </section>
+  );
+};
+
+export default PreviousPage;
