bcbe1ffee455ce264d68193c5e3eac6ecae7c6e8
Akanksha <akankshaprasad184@gmail.com>
2024-06-08 16:03:06 +0530

    fixed bugs

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -58,7 +58,7 @@ const MobileNav = () => {
                       )}
                     >
                       <Image
-                        src={link.imgUrl}
+                        src="icons/logo.svg"
                         alt={link.label}
                         width={20}
                         height={20}
