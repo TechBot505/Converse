d467eb5142d9f72f9207892e673203114d042ede
Rohit <rohitdhanotia2003@gmail.com>
2024-08-13 16:56:16 +0530

    Post Deployment Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,14 +28,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
-          },
-          variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#1C1F2E",
-            colorInputBackground: "#252A41",
-            colorInputText: "#fff",
+            logoImageUrl: "/icons/logo.png",
           },
         }}
       >
